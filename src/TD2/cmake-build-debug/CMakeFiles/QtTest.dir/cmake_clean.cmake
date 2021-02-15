be56a6872bce9566f068d06f4304336bbc112d22
Harsisis <56965488+Harsisis@users.noreply.github.com>
2021-02-15 08:00:40 +0100

    fichiers bizzares

---
+++ src/TD2/cmake-build-debug/CMakeFiles/QtTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QtTest.dir/main.cpp.obj"
+  "QtTest.exe"
+  "QtTest.exe.manifest"
+  "QtTest.pdb"
+  "libQtTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QtTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/TD2/cmake-build-debug/CMakeFiles/TD2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TD2.dir/main.cpp.obj"
+  "TD2.exe"
+  "TD2.exe.manifest"
+  "TD2.pdb"
+  "libTD2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TD2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
